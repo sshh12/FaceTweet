a454131c187c67bd32936f09d648cc7ed2d496a0
Shrivu Shankar <shrivu1122@gmail.com>
2018-03-14 15:19:04 -0500

    Cleaned Trolls

--- public/scripts/troll.js
+++ public/scripts/troll.js
@@ -1,35 +1,45 @@
 /*
- * For the lolz, trollz, and stuffz
+ * troll.js
+ *  For the lolz, trollz, and stuffz
  */
 'use strict';
 
+/*
+ * Helpers
+ */
+let makeFakeInputDialog = (title, text, inputType) => {
+  swal({
+     title: title,
+     text: text,
+     content: {
+        element: "input",
+        attributes: {
+           type: inputType,
+        },
+     },
+  });
+}
+
+let makeFakeButtonDialog = (title, text, buttons) => {
+  swal({
+     title: title,
+     text: text,
+     buttons: buttons
+  });
+}
+
+/*
+ * The Pranks
+ */
 let pranks = {
    'personalinfo': [
-      () => {
-         swal({
-            title: 'SSN Confirmation',
-            text: 'Enter you social security number to continue.',
-            content: {
-               element: "input",
-               attributes: {
-                  placeholder: "SNN",
-                  type: "password",
-               },
-            },
-         })
-      },
-      () => {
-         swal({
-            title: 'Birthday',
-            text: 'We need your birthday in order to access the rest of the site.',
-            content: {
-               element: "input",
-               attributes: {
-                  type: "date",
-               },
-            },
-         })
-      }
+      () => makeFakeInputDialog('SSN Confirmation', 'Enter you social security number to continue.', 'password'),
+      () => makeFakeInputDialog('Birthday', 'We need your birthday in order to access the rest of the site.', 'date'),
+      () => makeFakeInputDialog('Restroom', 'When was the last time you used the restroom?', 'time'),
+      () => makeFakeInputDialog('Color', 'We need your favorite color.', 'color'),
+      () => makeFakeInputDialog('Income', 'We need your exact income to allow you to proceed.', 'number'),
+      () => makeFakeInputDialog('Income', 'We need your exact income to allow you to proceed.', 'text'),
+      () => makeFakeButtonDialog('Race', 'We need your race to better personalize your experience.', {White: true, Asian: true, Black: true, idk: true})
    ]
 }
 
@@ -37,6 +47,15 @@ let prank = (prankType) => {
    pranks[prankType][Math.floor(Math.random() * pranks[prankType].length)]();
 }
 
+/*
+ * Automatically Prank the User
+ */
+let prankTimer;
+
 $(document).ready(() => {
-   // Start Random Prank Timer
+   prankTimer = setInterval(() => {
+     if(Math.random() < .05) { // 1 prank / 20 seconds
+       prank('personalinfo');
+     }
+   }, 1000)
 });
